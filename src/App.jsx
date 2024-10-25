6c2ec68c85753a4e6202cc007e59606839b6628b
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-25 19:58:40 -0400

    create responsive navbar(i think)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
 
   return (
       <>
-          <h1 className="text-2xl font-bold bg-amber-600 text-center mt-10">Test</h1>
+         <Navbar/>
       </>
   );
 }
