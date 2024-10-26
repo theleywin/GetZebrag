52f3dab9f0ffb307bca9d44b74ba9fdd1c9975c3
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-25 21:58:47 -0400

    put them on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Features from "./components/Features.jsx";
-
+import Workflow from "./components/Workflow.jsx";
+import Price from "./components/Price.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import Footer from "./components/Footer.jsx";
 const App = () => {
 
   return (
@@ -9,6 +12,10 @@ const App = () => {
          <Navbar/>
           <Hero/>
           <Features/>
+          <Workflow/>
+          <Price/>
+          <Testimonials/>
+          <Footer/>
       </>
   );
 }
