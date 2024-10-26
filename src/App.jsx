f36d19c266cda5cd859a49120f2f107f2021104b
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-25 21:00:02 -0400

    add Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
 
   return (
       <>
          <Navbar/>
+          <Hero/>
       </>
   );
 }
