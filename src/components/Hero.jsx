0372d4c666d191250c5c27104822ff2f7c8aaa64
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-10-25 21:13:30 -0400

    only use one video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
 
 const Hero = () => {
@@ -31,15 +30,7 @@ const Hero = () => {
                     autoPlay
                     loop
                     muted
-                    className="rounded-lg w-1/3 border border-orange-700 shadow-sm shadow-orange-400 mx-20 my-4">
-                    <source src={video1} type="video/mp4"/>
-                    Your browser does not support the video tag.
-                </video>
-                <video
-                    autoPlay
-                    loop
-                    muted
-                    className="rounded-lg w-1/3 border border-orange-700 shadow-sm shadow-orange-400 mx-20 my-4">
+                    className="rounded-lg border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4">
                     <source src={video2} type="video/mp4"/>
                     Your browser does not support the video tag.
                 </video>
