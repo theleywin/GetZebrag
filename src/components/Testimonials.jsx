d3d174707ddc184ec598e67263cb04e490583029
Pablo Gómez Vidal <123096717+theleywin@users.noreply.github.com>
2024-11-08 13:59:16 -0500

    test for discord integration

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -12,6 +12,7 @@ const Testimonials = () => {
                     <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
                         <div className="bg-neutral-900 rounded-md p-6 text-md border border-neutral-800 font-thin">
                             <p>{testimonial.text}</p>
+
                             <div className="flex mt-8 items-start">
                                 <img
                                     className="w-12 h-12 mr-6 rounded-full border border-neutral-300"
